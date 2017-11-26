598d676ff6178a1b9d7ef592d160c27ae359e60d
Kenneth Blix <kennethblix@gmail.com>
2017-11-26 23:21:54 +0100

    Made timer and method for StartGame

--- SnakeTheGame/SnakeTheGame/Form1.cs
+++ SnakeTheGame/SnakeTheGame/Form1.cs
@@ -12,10 +12,35 @@ namespace SnakeTheGame
 {
     public partial class Form1 : Form
     {
+        private List<Circle> Snake = new List<Circle>();
+        private Circle food = new Circle();
 
         public Form1()
         {
             InitializeComponent();
+
+            new Settings();
+
+            GameTimer.Interval = 1000 / Settings.Speed;
+            GameTimer.Tick += UpdateScreen;
+            GameTimer.Start();
+
+            //Start New Game
+            StarGame();
         }
+
+        private void StarGame()
+        {
+            new Settings();
+
+            Circle head = new Circle();
+            head.X = 10;
+            head.Y = 5;
+            Snake.Add(head);
+
+            LabelScore.Text = Settings.Score.ToString();
+            GenerateFood();
+        }
+
     }
 }
