cd0b05b5861ff26b6ee27aeda54ec46bdf55ce87
Kenneth Blix <kennethblix@gmail.com>
2017-11-27 00:09:27 +0100

    MovePlayer, eat and die

--- SnakeTheGame/SnakeTheGame/Form1.cs
+++ SnakeTheGame/SnakeTheGame/Form1.cs
@@ -56,6 +56,84 @@ namespace SnakeTheGame
             pbCanvas.Invalidate();
         }
 
+        private void MovePlayer()
+        {
+            for (int i = Snake.Count - 1; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+                    switch (Settings.direction)
+                    {
+                        case Direction.Up: Snake[i].Y++;
+                            break;
+                        case Direction.Down:Snake[i].Y--;
+                            break;
+                        case Direction.Left: Snake[i].X--;
+                            break;
+                        case Direction.Right: Snake[i].X++;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    //Get maximum X and Y position
+
+                    int maxXPos = pbCanvas.Size.Width / Settings.Width;
+                    int maxYPos = pbCanvas.Size.Height / Settings.Height;
+
+                    //Detect collission with game borders
+
+                    if (Snake[i].X < 0 || Snake[i].Y < 0 || Snake[i].X >= maxXPos || Snake[i].Y >= maxYPos)
+                    {
+                        Die();
+                    }
+
+                    for (int j = 0; j < Snake.Count; j++)
+                    {
+                        if (Snake[i].X == Snake[j].X && Snake[i].Y == Snake[j].Y)
+                        {
+                            Die();
+                        }
+                    }
+
+                    //Detect colliison with food piece
+                    if (Snake[0].X == food.X && Snake[0].Y == food.Y)
+                    {
+                        Eat();
+                    }
+                    
+
+                }
+                else
+                {
+                    //Move body
+                    Snake[i].X = Snake[i - 1].X;
+                    Snake[i].Y = Snake[i - 1].Y;
+                }
+            }
+
+        }
+
+        private void Eat()
+        {
+            Circle food = new Circle();
+            food.X = Snake[Snake.Count - 1].X;
+            food.Y = Snake[Snake.Count - 1].Y;
+
+
+            Snake.Add(food);
+
+            Settings.Score += Settings.Points;
+            LabelScore.Text = Settings.Score.ToString();
+
+            GenerateFood();
+        }
+
+        private void Die()
+        {
+            Settings.GameOver = true;
+        }
+
         private void StartGame()
         {
             new Settings();
@@ -120,4 +198,7 @@ namespace SnakeTheGame
             }
         }
     }
+
+
+
 }
